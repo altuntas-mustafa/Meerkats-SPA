b13d44cb8957bf3ad11ad5de59b132419275e64c
salt-dev <salt-dev@salt-devs-MacBook-Air-3.local>
2023-02-20 12:10:25 +0100

    Installation partially done

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.ts'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/index.html'), 
+      filename: 'index.html', // output file
+    }),
+    new CleanWebpackPlugin(),
+  ]
+}
