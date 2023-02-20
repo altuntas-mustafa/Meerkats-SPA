72a235ef5e24cfdf14cbe1d24b6fa9777084f28d
salt-dev <salt-dev@salt-devs-MacBook-Air-3.local>
2023-02-20 15:03:36 +0100

    webpack until images done

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+const mainImage = require('./images/download.jpeg')
+// import mainImage from 
 // Create heading node
 const heading = document.createElement('h1')
-heading.textContent = 'Interesting!'
 
+let container = document.querySelector('#root')!
+const image = document.createElement('img')
+container.appendChild(image)
+image.src = mainImage;
+
+heading.textContent = 'Interesting!'
 // Append heading node to the DOM
 const app = document.querySelector('#root')!
 app.append(heading)
+app.append(image)
