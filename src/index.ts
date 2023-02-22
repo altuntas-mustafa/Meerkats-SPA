9f0b00a90321fea50bf05ae1f7bc7262d352e5df
salt-dev <salt-dev@salt-devs-MacBook-Pro-5.local>
2023-02-22 10:01:18 +0100

    Images can be searched on our input. Yey!

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,69 @@
 import './styles/main.scss'
+import { createApi } from 'unsplash-js';
+import { search } from 'unsplash-js/dist/internals';
 
-const accessKey = 'W3ZYLO4p0TjuNtGtCxuUxd-VQjISiTJdPPGkQFDXz3c'
-console.log(accessKey);
-const input = document.querySelector('.input-field')!;
+// const accessKey = 'W3ZYLO4p0TjuNtGtCxuUxd-VQjISiTJdPPGkQFDXz3c'
+const inputField = document.querySelector('.field') as HTMLInputElement;
+const getQuery = document.querySelector('.button-search')!;
+let value = ''
+getQuery.addEventListener('click',() => {
+  value = inputField.value;
+  getUnsplashPhotos(value) 
+  console.log(value);
+})
 
-const apiUrl: string = 'https://api.unsplash.com/search/photos';
-const headers: Headers = new Headers({
-  'Authorization': `Client-ID ${accessKey}`,
-});
-
-fetch(`${apiUrl}?page=1&query=car&client_id=W3ZYLO4p0TjuNtGtCxuUxd-VQjISiTJdPPGkQFDXz3c`, { headers })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("object");
-    console.trace(data);
-    const photoUrls: string[] = data.results.map((photo: any) => photo.urls.regular);
-    const selectedPhotos: string[] = [];
-
-    while (selectedPhotos.length < 2) {
-      const randomIndex = Math.floor(Math.random() * photoUrls.length);
-      const randomPhoto = photoUrls[randomIndex];
-      if (!selectedPhotos.includes(randomPhoto)) {
-        selectedPhotos.push(randomPhoto);
-      }
-    }
-    selectedPhotos.forEach((photoUrl) => {
-      const imgElement: HTMLImageElement = document.createElement('img');
-      imgElement.src = photoUrl;
-      document.body.appendChild(imgElement);
-    });
-  })
-  .catch((error) => console.error(error));
-// ------ Webpack add image with asset/resource -----
 const mainImage = require('./images/download.jpeg')
 const logoContainer = document.querySelector('.logo')!
 const image = document.createElement('img')
 logoContainer.appendChild(image)
 image.src = mainImage;
 // ---------end -----
+
+const unsplash = createApi({
+  accessKey: 'W3ZYLO4p0TjuNtGtCxuUxd-VQjISiTJdPPGkQFDXz3c',
+  // headers: { 'X-Custom-Header': 'foo' },
+});
+function getUnsplashPhotos(value : string) {
+  unsplash.search.getPhotos({
+    query: `${value}`,
+    page: 1,
+    perPage: 6
+  })
+  .then(data => {
+    console.log(data);
+    const imageContainer = document.querySelector('.image-section')!
+    imageContainer.innerHTML = ''
+    data.response?.results.forEach(photo => {
+      const photoUrl = photo.urls.regular;
+      const imgElement: HTMLImageElement = document.createElement('img');
+      imgElement.src = photoUrl;
+      imageContainer.appendChild(imgElement);
+    })
+  })
+}
+// fetch(`${apiUrl}?page=1&query=car&client_id=W3ZYLO4p0TjuNtGtCxuUxd-VQjISiTJdPPGkQFDXz3c`, { headers })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const photoUrls: string[] = data.results.map((photo: any) => photo.urls.regular);
+//     const selectedPhotos: string[] = [];
+
+//     while (selectedPhotos.length < 2) {
+//       const randomIndex = Math.floor(Math.random() * photoUrls.length);
+//       const randomPhoto = photoUrls[randomIndex];
+//       if (!selectedPhotos.includes(randomPhoto)) {
+//         selectedPhotos.push(randomPhoto);
+//       }
+//     }
+//     selectedPhotos.forEach((photoUrl) => {
+//       const imgElement: HTMLImageElement = document.createElement('img');
+//       imgElement.src = photoUrl;
+//       document.body.appendChild(imgElement);
+//     });
+//   })
+//   .catch((error) => console.error(error));
+// ------ Webpack add image with asset/resource -----
+// const headers: Headers = new Headers({
+  //   'Authorization': `Client-ID ${accessKey}`,
+  // });
+  
+// const apiUrl: string = 'https://api.unsplash.com/search/photos';
